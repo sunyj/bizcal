d36febc9b8977d2be33734f96f9baf8908f1663c
sunyj <sunyijiang@gmail.com>
2024-07-02 18:02:59 +0800

    First working version.

--- bizcal/__init__.py
+++ bizcal/__init__.py
@@ -1,43 +1,65 @@
-__all__ = ['Day', 'BizDay', 'Calendar']
+__all__ = ['Calendar']
 
+import re
 import datetime as pydt
 
 
-class Day:
-    def __init__(self, days=1):
-        self.days = days
+class Date(pydt.date):
+    def __new__(cls, y, m, d, cal, idx=None, holiday=None, biz=None):
+        day = pydt.date.__new__(cls, y, m, d)
+        day.cal = cal
+        day.idx = cal._idx(day) if idx is None else idx
+        if holiday is None:
+            day.holiday = day.month * 100 + day.day in cal.table[day.idx]
+        else:
+            day.holiday = holiday
+        day.open = not day.holiday and day.weekday() < 5 if biz is None else biz
+        return day
 
-class BizDay:
-    def __init__(self, days=1):
-        self.days = days
 
-
-class Date:
-    def __init__(self, y, m, d, holiday=False):
-        self.d = pydt.date(y, m, d)
-        self.holiday = holiday
-
-    @property
-    def open(self): return self.d.weekday() < 5 and not self.holiday
+    def clone(self):
+        return Date(self.year, self.month, self.day, self.cal,
+                    self.idx, self.holiday, self.open)
 
     @property
     def num(self): return self.year * 10000 + self.month * 100 + self.day
 
-    @property
-    def str(self): return self.strftime('%Y%m%d')
+    def __eq__(self, d):
+        if isinstance(d, Date):
+            return self.num == d.num
+        return (self.year == d.year and self.month == d.month
+                and self.day == d.day)
+
 
     def __add__(self, days):
-        d = self.d + pydt.timedelta(days)
-        return Date(d.year, d.month, d.day)
+        "Shift calendar days."
+        if not days:
+            return self.clone()
+        d = pydt.date.fromordinal(self.toordinal() + days)
+        return Date(d.year, d.month, d.day, self.cal,
+                    self.idx if d.year == self.year else None)
 
     def __sub__(self, days):
-        d = self.d - pydt.timedelta(days)
-        return Date(d.year, d.month, d.day)
+        return self + (-days)
 
-    def __str__(self):
-        tag = '*' if self.holiday else ''
-        return self.d.strftime(f'%Y.%m.%d{tag}')
-    __repr__ = __str__
+
+    def __rshift__(self, days):
+        "Shift business days."
+        return self.shift(days)
+
+    def __lshift__(self, days):
+        return self.shift(-days)
+
+    def shift(self, days):
+        if not days:
+            return self.clone()
+        delta = days > 0 and 1 or -1
+        days = abs(days)
+        day = self.clone()
+        while days:
+            day = day + delta
+            days -= (day.open and 1 or 0)
+        return day
 
 
 class Calendar:
@@ -49,32 +71,102 @@ class Calendar:
         for line in spec:
             s = line.split(':')
             cals[int(s[0])] = set(x for spec in s[1].split(',')
-                                  for x in parse_hhmm(int(s[0]), spec))
+                                  for x in parse_mmdd(int(s[0]), spec))
         if not cals:
             raise ValueError('invalid calendar spec')
         years = sorted(cals.keys())
-        self.since = years[0]
-        self.until = years[-1]
-        if self.until - self.since + 1 < len(years):
+        self.ymin = years[0]
+        self.ymax = years[-1]
+        if self.ymax - self.ymin + 1 < len(years):
             raise ValueError('incomplete calendar spec')
         self.table = [cals[y] for y in years]
+        assert len(self.table) == self.ymax - self.ymin + 1
+
+    def _parse_date(self, spec):
+        if isinstance(spec, str):
+            return self._parse_date(int(re.sub(r'\D', '', spec)))
+        if isinstance(spec, (list, tuple)):
+            return self._parse_date(pydt.date(*spec))
+        if isinstance(spec, pydt.date):
+            d = spec
+        else:
+            if spec < 19000000 or spec > 29999999:
+                raise ValueError(f'invalid date number {spec}')
+            year = spec // 10000
+            mmdd = spec % 10000
+            d = pydt.date(year, mmdd // 100, mmdd % 100)
+        return (d, self._idx(d))
+
+    def _idx(self, d):
+        if d.year < self.ymin or d.year > self.ymax:
+            raise ValueError(f'{d} outside [{self.ymin}, {self.ymax}]')
+        return d.year - self.ymin
+
+    def is_holiday(self, spec):
+        d, idx = self._parse_date(spec)
+        return d.month*100 + d.day in self.table[idx]
 
 
-def parse_hhmm(year, spec):
+    def is_open(self, spec):
+        d, idx = self._parse_date(spec)
+        return d.weekday() < 5 and d.month * 100 + d.day not in self.table[idx]
+
+    def __contains__(self, spec):
+        return self.is_open(spec)
+
+
+    def day(self, *args):
+        if len(args) == 3:
+            return Date(args[0], args[1], args[2], self)
+        if len(args) == 1:
+            d, idx = self._parse_date(args[0])
+            return Date(d.year, d.month, d.day, self, idx)
+
+    def __call__(self, *spec):
+        return self.day(*spec)
+
+
+    def bizdays(self, since, until=None):
+        "Generate business days."
+
+        if until is None:
+            if not isinstance(since, str):
+                raise TypeError('only string allowed in single-param bizdays')
+            since, until = parse_range(since)
+        day, _ = self._parse_date(since)
+        end, _ = self._parse_date(until)
+        one = pydt.timedelta(1)
+        while day <= end:
+            if self.is_open(day):
+                yield day
+            day += one
+
+
+def parse_mmdd(year, spec):
     if '-' not in spec:
         yield int(spec)
         return
-    s = spec.strip().split('-')
-    if len(s) > 2:
-        raise ValueError(f'invalid period spec {spec}')
-    beg = int(s[0])
-    end = int(s[0][:-len(s[1])] + s[1])
+    beg, end = parse_range(spec, numeric=True)
     day = pydt.date(year, beg // 100, beg % 100)
     end = pydt.date(year, end // 100, end % 100)
     oneday = pydt.timedelta(1)
     while day <= end:
         yield day.month * 100 + day.day
         day = day + oneday
+
+
+def parse_range(spec, numeric=False):
+    if numeric:
+        s = parse_range(spec, False)
+        return (int(s[0]), int(s[1]))
+    s = spec.strip().split('-')
+    if len(s) > 2:
+        raise ValueError(f'invalid range spec {spec}')
+    if len(s) < 2:
+        return (spec, spec)
+    if len(s[0]) < len(s[1]):
+        return (s[0], s[1])
+    return (s[0], s[0][:-len(s[1])] + s[1])
 
 
 ### bizcal/__init__.py ends here
--- test/basic.py
+++ test/basic.py
@@ -1,11 +1,25 @@
 import unittest, json
 from bizcal import *
+from bizcal import parse_range
+
 
 class TestBasic(unittest.TestCase):
 
-    def test_spec_read(self):
-        spec = ['2023: 0101-2, 0121-27, 0405, 0429-0503, 0622-24, 0929-1006',
-                '2024: 0101, 0210-7, 0404-6, 0501-5, 0610, 0915-7, 1001-7']
+    def test_parse_range(self):
+        with self.assertRaises(ValueError):
+            parse_range('1-2-3')
+        self.assertEqual(parse_range('abc'), ('abc', 'abc'))
+        self.assertEqual(parse_range('abc-1234'), ('abc', '1234'))
+        self.assertEqual(parse_range('1234-56'), ('1234', '1256'))
+        self.assertEqual(parse_range('1234-4567'), ('1234', '4567'))
+        self.assertEqual(parse_range('1234-12345'), ('1234', '12345'))
+        self.assertEqual(parse_range('1234-5'), ('1234', '1235'))
+        self.assertEqual(parse_range(''), ('', ''))
+
+
+    def test_spec(self):
+        spec = ['2023: 0101-2,0121-27,0405,0429-0503,0622-24,0929-1006',
+                '2024: 0101,0210-7,0404-6,0501-5,0610,0915-7,1001-7']
         s = Calendar(spec)
         self.assertEqual(len(s.table), 2)
         self.assertEqual(len(s.table[0]), 26)
@@ -15,5 +29,72 @@ class TestBasic(unittest.TestCase):
         self.assertEqual(len(s.table), 2)
         self.assertEqual(len(s.table[0]), 2)
         self.assertEqual(len(s.table[1]), 3)
+
+
+    def test_open(self):
+        cal = Calendar(['2024: 0101,0210-7,0404-6,0501-5,0610,0915-7,1001-7'])
+        with self.assertRaises(ValueError):
+            cal.is_open(20230701)
+        with self.assertRaises(ValueError):
+            cal.is_open(20990701)
+        self.assertTrue(20240701 in cal)
+        self.assertTrue(20240707 not in cal)
+        self.assertFalse(cal.is_open(20240501))
+
+
+    def test_holiday(self):
+        cal = Calendar(['2024: 0101,0210-7,0404-6,0501-5,0610,0915-7,1001-7'])
+        with self.assertRaises(ValueError):
+            cal.is_open(20230501)
+        with self.assertRaises(ValueError):
+            cal.is_open(20990501)
+        self.assertTrue(cal.is_holiday(20240501))
+        self.assertTrue(cal.is_holiday(20240101))
+        self.assertFalse(cal.is_holiday(20241231))
+        self.assertTrue(cal.is_holiday([2024, 5, 1]))
+        self.assertTrue(cal.is_holiday((2024, 1, 1)))
+
+
+    def test_bizdays(self):
+        cal = Calendar(['2024: 0101,0210-7,0404-6,0501-5,0610,0915-7,1001-7'])
+        days = list(cal.bizdays(20240101, '2024-01.05'))
+        self.assertEqual(len(days), 4)
+        self.assertEqual(days[-1].strftime('%Y%m%d'), '20240105')
+        self.assertEqual(list(cal.bizdays(20240210, 20240215)), [])
+        self.assertEqual(len(list(cal.bizdays('20240101-5'))), 4)
+        self.assertEqual(len(list(cal.bizdays('20240901-30'))), 19)
+        self.assertEqual(list(cal.bizdays('20240101')), [])
+
+
+    def test_date(self):
+        cal = Calendar(['2024: 0101,0210-7,0404-6,0501-5,0610,0915-7,1001-7'])
+        day = cal(20240101)
+        self.assertEqual(day.idx, 0)
+        self.assertFalse(day.open)
+        self.assertFalse(cal(2024, 5, 1).open)
+        self.assertTrue(cal(20240501).holiday)
+
+    def test_cal_shift(self):
+        cal = Calendar(['2024: 0101,0210-7,0404-6,0501-5,0610,0915-7,1001-7'])
+        day = cal(20240101) + 1
+        self.assertTrue(day.open)
+        self.assertEqual(day.strftime('%Y%m%d'), '20240102')
+
+        day = cal(20240228) + 1
+        self.assertTrue(day.open)
+        self.assertEqual(day.strftime('%Y%m%d'), '20240229')
+        day = day - 1
+        self.assertEqual(day.strftime('%Y%m%d'), '20240228')
+
+
+    def test_biz_shift(self):
+        cal = Calendar(['2024: 0101,0210-7,0404-6,0501-5,0610,0915-7,1001-7'])
+        d = cal(20240101) >> 10
+        self.assertEqual(d.strftime('%Y%m%d'), '20240115')
+        d = d << 9
+        self.assertEqual(d.strftime('%Y%m%d'), '20240102')
+        with self.assertRaises(ValueError):
+            d << 10
+
 
 ### test/basic.py ends here
